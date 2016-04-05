71ea715d64db97cceba0e4d7fe574adf6c9f66f3
Robert Nyquist <nyquist@student.chalmers.se>
2016-04-06 01:01:50 +0200

    added navigation, does not work properly

--- cube.py
+++ cube.py
@@ -81,7 +81,26 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        glRotatef(1,3,1,1)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1: #use 4 for scroll
+                    glTranslatef(0,0,0.1)
+
+                if event.button == 3: #use 5 for scroll
+                    glTranslatef(0,0,-0.1)
+		
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    glTranslatef(-0.5,0,0)
+                if event.key == pygame.K_RIGHT:
+                    glTranslatef(0.5,0,0)
+
+                if event.key == pygame.K_UP:
+                    glTranslatef(0,1,0)
+                if event.key == pygame.K_DOWN:
+                    glTranslatef(0,-1,0)
+
+       	glRotatef(1,3,1,1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
